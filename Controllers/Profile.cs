cda75dd6f1895af9334db3b8c3121e273418d7c4
glwhitaker <glwhitaker3@gmail.com>
2024-03-08 17:27:09 -0500

    Add project files.

---
+++ Controllers/Profile.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TigerTix.Controllers
+{
+    public class Profile : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
